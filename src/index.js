a26d5a3bbb42e8f8ada6b8a0cfd41feb7a71fd81
Szymon Guzik <szymon198198@gmail.com>
2020-07-27 20:53:50 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import bodyParser from 'body-parser';
+
+import chalk from 'chalk';
+
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+/* eslint no-console: 0 */
+
+const port = process.env.port || 8080;
+
+app.listen(port, () =>
+	console.log(chalk.green('[Info]'), `Test app listening on port ${chalk.blue.bold(port)}!`)
+);
